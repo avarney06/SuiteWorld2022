7b512222734a51e28f6c617380cee597af488c8f
Adam Varney <avarney06@gmail.com>
2022-07-29 13:25:06 -0400

    HelloWorld Suitelet

---
+++ SDFProjects/SimpleSuitelet/src/FileCabinet/SuiteScripts/sl_helloworld.js
@@ -0,0 +1,90 @@
+/** 
+ *@NApiVersion 2.1
+ *@NScriptType Suitelet
+*/
+/*jshint esversion: 9*/
+define([
+    'N/ui/serverWidget',
+    'N/record',
+    'N/search'
+],
+    (serverWidget, record, search) => {
+        const onRequest = context => {
+            log.debug('Context', context);
+            let form = serverWidget.createForm({
+                title: 'Hello World',
+                hideNavbar: false
+            });
+            let parameters = context.request.parameters;
+            log.debug('Parameters', parameters);
+            if (context.request.method === 'GET') {
+                let customerId = parameters.customerid;
+                buildGetForm(form, customerId);
+            } else {
+                let helloText = parameters.custpage_hello;
+                let customerId = parameters.customerid;
+                record.submitFields({
+                    type: record.Type.CUSTOMER,
+                    id: customerId,
+                    values: {
+                        comments: helloText
+                    }
+                });
+                let customerLookup = getCustomerDetails(customerId);
+                buildPostForm(form, customerLookup);
+            }
+            context.response.writePage(form);
+            return;
+        };
+
+        const buildGetForm = (form, customerId) => {
+            let customerLookup = getCustomerDetails(customerId);
+            log.debug('Customer Lookup', customerLookup);
+            form.addField({
+                id: 'custpage_comments',
+                type: serverWidget.FieldType.TEXTAREA,
+                label: 'Current Comments'
+            })
+            .updateDisplayType({
+                displayType: serverWidget.FieldDisplayType.INLINE
+            })
+            .defaultValue = customerLookup.comments;
+            form.addField({
+                id: 'custpage_hello',
+                label: 'Hello Text',
+                type: serverWidget.FieldType.TEXT
+            })
+            .defaultValue = 'Hello World';
+            form.addSubmitButton({
+                label: 'Save'
+            });
+        };
+
+        const buildPostForm = (form, customerLookup) => {
+            form.addField({
+                id: 'custpage_success',
+                label: 'Success',
+                type: serverWidget.FieldType.TEXT
+            })
+            .updateDisplayType({
+                displayType: serverWidget.FieldDisplayType.INLINE
+            })
+            .defaultValue = `Customer Id ${customerLookup.internalid[0].value} was saved successfully.`;
+        };
+
+        const getCustomerDetails = customerId => {
+            return search.lookupFields({
+                type: search.Type.CUSTOMER,
+                id: customerId,
+                columns: [
+                    'comments',
+                    'internalid'
+                ]
+            });
+        }; 
+
+        return {
+            onRequest:onRequest
+        };
+    }
+);
